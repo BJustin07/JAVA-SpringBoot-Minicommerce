9de7e4e50ceed4dcebdb4ac7d5dab5beb5e45a2c
BJustin07 <baquiranjustin07@gmail.com>
2026-05-31 15:14:29 +0800

    init

---
+++ src/test/java/com/medina/mini_commerce/MiniCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medina.mini_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
